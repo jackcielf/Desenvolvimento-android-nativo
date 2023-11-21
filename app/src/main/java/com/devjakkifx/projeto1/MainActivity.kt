2a2c8201e7a4b461cad87644783734433f83c642
JakkiFx <jackcielfelix555@gmail.com>
2023-11-21 11:11:56 -0300

    app de sorteio

--- app/src/main/java/com/devjakkifx/projeto1/MainActivity.kt
+++ app/src/main/java/com/devjakkifx/projeto1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.devjakkifx.projeto1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun click(view: View) {
-        println("Foi clicado!");
+    fun sortear(view: View) {
+        val txtResult = findViewById<TextView>(R.id.text_result); // Seleciona um elemento da interface pelo ID
+        val number = Random().nextInt(10); // Gera um número inteiro aleatório, no caso, de 0 a 10
+        txtResult.setText("Número gerado: $number");
     }
 }
