3010b43ec444f47c33cd77846c87aaaf827de5d4
JakkiFx <jackcielfelix555@gmail.com>
2023-11-21 10:48:40 -0300

    evento de clique

--- app/src/main/java/com/devjakkifx/projeto1/MainActivity.kt
+++ app/src/main/java/com/devjakkifx/projeto1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.devjakkifx.projeto1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun click(view: View) {
+        println("Foi clicado!");
     }
 }
